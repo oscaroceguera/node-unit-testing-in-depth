2005fb0eb77fe8898ec632c32d84f6a0e450e2dc
oscaroceguera <oscaroceguera@gmail.com>
2018-06-05 17:30:18 -0500

    Enviromental variables & cross-env

--- mocha-chai-setup/test/lib/v1/bar.test.js
+++ mocha-chai-setup/test/lib/v1/bar.test.js
@@ -17,6 +17,10 @@ describe('file to be tested', () => {
     it('should do something', () => {
       assert.equal(1, 1)
       console.log('ENV', process.env.NODE_ENV)
+
+      if (process.env.NODE_ENV === 'development') {
+        console.log('DEVELOPMENT NODE')
+      }
     })
     it('should do something else', () => {
       assert.deepEqual({ name: 'joe' }, { name: 'joe' })
